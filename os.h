b823712dc073d70873a1b6201c5433c286d87cc8
Rafat Mahmud <rafatm@uvic.ca>
2018-03-15 11:58:32 -0700

    Fix wrong interrupt headers.

--- os.h
+++ os.h
@@ -126,8 +126,8 @@ unsigned int Now();  // number of milliseconds since the RTOS boots.
   * inline assembly code to disable/enable maskable interrupts   
   * (N.B. Use with caution.)  
   */  
-#define OS_DI()    asm(" sei ")  /* disable all interrupts */  
-#define OS_EI()    asm(" cli ")  /* enable all interrupts */  
+#define OS_EI()    asm(" sei ")  /* disable all interrupts */
+#define OS_DI()    asm(" cli ")  /* enable all interrupts */
 
 
 /**
